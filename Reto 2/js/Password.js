123e2a46e9cd429f2756424806fbe3a15d249a22
Joel Tarzán <45524750+JoelTarzan@users.noreply.github.com>
2023-06-26 18:48:52 +0200

    Refactor Reto 2

--- Reto 2/js/Password.js
+++ Reto 2/js/Password.js
@@ -36,9 +36,3 @@ class Password {
         this.caracteres = newCaracteres;
     }
 }
-// Instancias de ejemplo
-let password1 = new Password();
-console.log('Contraseña antes de cambiar la longitud: ' + password1.getContrasena());
-password1.setLongitud(12);
-password1.newContrasena();
-console.log('Contraseña después de cambiar la longitud: ' + password1.getContrasena());
---
+++ Reto 2/src/main.ts
@@ -0,0 +1,7 @@
+// Instancias de ejemplo
+let password1 = new Password();
+console.log('Contraseña antes de cambiar la longitud: ' + password1.getContrasena());
+
+password1.setLongitud(12);
+password1.newContrasena();
+console.log('Contraseña después de cambiar la longitud: ' + password1.getContrasena());
